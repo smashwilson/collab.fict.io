d07664f55f135181dcd45d9b9dd167a2bb39b004
Ash Wilson <smashwilson@gmail.com>
2015-01-23 13:37:25 -0500

    Generate snippet and story resources.

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource("stories", function() {});
+  this.resource("snippets", function() {});
 });
 
 export default Router;
