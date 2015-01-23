52d350302b1b1faf75d91c68b30a7a76d4141bf1
Ash Wilson <smashwilson@gmail.com>
2015-01-23 11:54:59 -0500

    Import Bower assets.

--- Brocfile.js
+++ Brocfile.js
@@ -4,17 +4,20 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-// Use `app.import` to add additional libraries to the generated
-// output files.
-//
-// If you need to use different assets in different
-// environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
-// should be the asset to use in that environment.
-//
-// If the library that you are including contains AMD or ES6
-// modules that you would like to import into your application
-// please specify an object with the list of modules as keys
-// along with the exports of each module as its value.
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot');
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg');
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf');
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff');
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2');
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+
+app.import('bower_components/fontawesome/css/font-awesome.css');
+app.import('bower_components/fontawesome/fonts/fontawesome-webfont.eot');
+app.import('bower_components/fontawesome/fonts/fontawesome-webfont.svg');
+app.import('bower_components/fontawesome/fonts/fontawesome-webfont.ttf');
+app.import('bower_components/fontawesome/fonts/fontawesome-webfont.woff');
+app.import('bower_components/fontawesome/fonts/fontawesome-webfont.woff2');
+app.import('bower_components/fontawesome/fonts/FontAwesome.otf');
 
 module.exports = app.toTree();
