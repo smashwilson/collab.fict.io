2edaf8176ec11091d6207c7915a8f9489b40c4ff
Ash Wilson <smashwilson@gmail.com>
2015-01-23 12:00:30 -0500

    Relationship between Story and Snippets.

--- app/models/snippet.js
+++ app/models/snippet.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   content: DS.attr('string'),
   creatorEmail: DS.attr('string'),
-  createdAt: DS.attr('date')
+  createdAt: DS.attr('date'),
+  story: DS.belongsTo('story')
 });
--- app/models/story.js
+++ app/models/story.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   creatorEmail: DS.attr('string'),
-  createdAt: DS.attr('date')
+  createdAt: DS.attr('date'),
+  snippets: DS.hasMany('snippet')
 });
