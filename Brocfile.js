c184f2dca935b0e77cc321e3fbec716a6343132f
Ash Wilson <smashwilson@gmail.com>
2015-01-26 10:44:35 -0500

    Import FontAwesome assets correctly.

--- Brocfile.js
+++ Brocfile.js
@@ -5,19 +5,14 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot');
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg');
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf');
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff');
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2');
 app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
 app.import('bower_components/fontawesome/css/font-awesome.css');
-app.import('bower_components/fontawesome/fonts/fontawesome-webfont.eot');
-app.import('bower_components/fontawesome/fonts/fontawesome-webfont.svg');
-app.import('bower_components/fontawesome/fonts/fontawesome-webfont.ttf');
-app.import('bower_components/fontawesome/fonts/fontawesome-webfont.woff');
-app.import('bower_components/fontawesome/fonts/fontawesome-webfont.woff2');
-app.import('bower_components/fontawesome/fonts/FontAwesome.otf');
+app.import('bower_components/fontawesome/fonts/fontawesome-webfont.eot', { destDir: 'fonts' });
+app.import('bower_components/fontawesome/fonts/fontawesome-webfont.svg', { destDir: 'fonts' });
+app.import('bower_components/fontawesome/fonts/fontawesome-webfont.ttf', { destDir: 'fonts' });
+app.import('bower_components/fontawesome/fonts/fontawesome-webfont.woff', { destDir: 'fonts' });
+app.import('bower_components/fontawesome/fonts/fontawesome-webfont.woff2', { destDir: 'fonts' });
+app.import('bower_components/fontawesome/fonts/FontAwesome.otf', { destDir: 'fonts' });
 
 module.exports = app.toTree();
