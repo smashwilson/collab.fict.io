9212a1e321593714577b33b375b7bfed2eae7f5f
Ash Wilson <smashwilson@gmail.com>
2015-01-27 16:01:41 -0500

    Create the snippet, then the draft.

--- app/routes/stories/new.js
+++ app/routes/stories/new.js
@@ -3,15 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     submit: function () {
-      var content = this.controller.get('firstSnippet');
-
-      var snippet = this.store.createRecord('snippet', {
-        content: content,
+      var
+        self = this,
+        content = this.controller.get('firstSnippet'),
+        snippet = this.store.createRecord('snippet', {
+          content: content,
       });
 
       snippet.save().then(function () {
-        var draft = this.store.createRecord('draft', {
-          firstLine: snippet.slice(0, 30),
+        var draft = self.store.createRecord('draft', {
+          firstLine: content.slice(0, 30),
           contributionStatus: 'cooldown',
           canAdmin: true,
           snippetCount: 1,
@@ -20,7 +21,7 @@ export default Ember.Route.extend({
 
         return draft.save();
       }).then(function () {
-        this.transitionTo('index');
+        return self.transitionTo('index');
       });
     }
   }
