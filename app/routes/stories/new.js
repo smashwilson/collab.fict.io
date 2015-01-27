4e3ae42dfa735bc55500da5187e658c58ddf8090
Ash Wilson <smashwilson@gmail.com>
2015-01-27 16:03:13 -0500

    Create the Draft server-side instead.

--- app/routes/stories/new.js
+++ app/routes/stories/new.js
@@ -11,16 +11,6 @@ export default Ember.Route.extend({
       });
 
       snippet.save().then(function () {
-        var draft = self.store.createRecord('draft', {
-          firstLine: content.slice(0, 30),
-          contributionStatus: 'cooldown',
-          canAdmin: true,
-          snippetCount: 1,
-          lastSnippet: snippet,
-        });
-
-        return draft.save();
-      }).then(function () {
         return self.transitionTo('index');
       });
     }
